f87e4bec80c8aeac3b666b8581b07dab25fbd087
David928 <david.gonzales928@gmail.com>
2018-08-23 16:14:25 +0200

    first commit

---
+++ ColorBee/Scene/GameScene.swift
@@ -0,0 +1,24 @@
+//
+//  GameScene.swift
+//  ColorBee
+//
+//  Created by GONZALES David on 23/08/2018.
+//  Copyright © 2018 GONZALES David. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+
+    override func didMove(to view: SKView) {
+
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+      
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+    }
+}
