4a0e80d4d13135a23d567e70779633d5bae6bcee
David928 <david.gonzales928@gmail.com>
2018-08-23 16:50:32 +0200

    Ajout du sol

---
+++ ColorBee/Ressource/Constantes.swift
@@ -0,0 +1,13 @@
+//
+//  Constantes.swift
+//  ColorBee
+//
+//  Created by GONZALES David on 23/08/2018.
+//  Copyright © 2018 GONZALES David. All rights reserved.
+//
+
+import UIKit
+
+let MASK_JOUEUR: UInt32 = 1
+let MASK_OBSTACLE: UInt32 = 2
+let MASK_SOL: UInt32 = 4
